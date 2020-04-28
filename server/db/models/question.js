77843812a1f22d7f1ede746e8bc28d7f3403f80a
cofuente <contact@cofuente.io>
2020-04-28 16:45:20 -0400

    updated question model

--- server/db/models/question.js
+++ server/db/models/question.js
@@ -4,12 +4,17 @@ const db = require('../_db')
 
 const Question = db.define('question', {
   questionUUID: {
+    primaryKey: true,
     type: Sequelize.UUID,
-    defaultValue: Sequelize.UUIDV1, // or Sequelize.UUIDV4, this is to make sequelize generate the ids automatically
+    defaultValue: Sequelize.UUIDV4, // or Sequelize.UUIDV4, this is to make sequelize generate the ids automatically
     unique: true
   },
   questionType: {
-    type: Sequelize.ENUM('multiplechoice', 'checkbox', 'paragraph'), // for purposes of rendering the form accordingly in the front end
+    type: Sequelize.ENUM(
+      'multiplechoice',
+      'checkbox',
+      'paragraph',
+      'input'), // for purposes of rendering the form accordingly in the front end
     allowNull: false,
   },
   questionPrompt: {
@@ -18,8 +23,30 @@ const Question = db.define('question', {
   },
   possibleAnswers: {
     type: Sequelize.ARRAY(Sequelize.TEXT),
-    defaultValue: ['']
+    defaultValue: [
+      'n/a'
+    ]
   }
 })
 
 module.exports = Question
+
+/* // It is possible to create foreign keys:
+  bar_id: {
+    type: DataTypes.INTEGER,
+
+    references: {
+      // This is a reference to another model
+      model: Bar,
+
+      // This is the column name of the referenced model
+      key: 'id',
+
+      // With PostgreSQL, it is optionally possible to declare when to check the foreign key constraint, passing the Deferrable type.
+      deferrable: Deferrable.INITIALLY_IMMEDIATE
+      // Options:
+      // - `Deferrable.INITIALLY_IMMEDIATE` - Immediately check the foreign key constraints
+      // - `Deferrable.INITIALLY_DEFERRED` - Defer all foreign key constraint check to the end of a transaction
+      // - `Deferrable.NOT` - Don't defer the checks at all (default) - This won't allow you to dynamically change the rule in a transaction
+    }
+  }, */
