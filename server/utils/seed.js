172e21c90f8f4089f1d0da374209bae2b396f370
cofuente <contact@cofuente.io>
2020-04-28 22:26:31 -0400

    added form and question seeds

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -9,36 +9,103 @@ async function seed() {
 
   const questions = await Promise.all([
     Question.create({
-      questionType: 'multiplechoice',
-      questionPrompt: 'what is your gender?',
+      // Note the UUID's we are pre-assigning our two basic questions here. These strings are not necessary to add questions to the database as Sequelize automatically generates unique UUID's for every new question, but we are making an exception here so that we may refer to that unique Question model instance by its primary key. We will pass these primary keys into various forms' addQuestion() methods later.
+      questionUUID: '73c7ed89-8c6f-4e6e-8d19-ca0250b37cb0',
+      questionType: 'checkbox',
+      questionPrompt: 'Do you solemnly swear to the best of your knowledge that all of these answers are correct?',
       possibleAnswers: [
-        'male-ish',
-        'female-ish',
-        'NB',
-        'HAHAHAHA!'
+        'checked'
       ]
     }),
     Question.create({
-      questionType: 'input',
-      questionPrompt: 'what is your name?',
-    }),
-    Question.create({
-      questionType: 'paragraph',
-      questionPrompt: 'what is your address?',
-    }),
-    Question.create({
-      questionType: 'checkbox',
-      questionPrompt: 'do you verify your answers in this form are all true to the best of your knowledge?',
+      questionUUID: '1ac4b921-0568-4fcc-af89-bf46b64f4068',
+      questionType: 'multiplechoice',
+      questionPrompt: 'What is the airspeed velocity of an unladen swallow?',
       possibleAnswers: [
-        'checked']
-    }),
+        'Depends. Are you refering to the African unladen swallow?',
+        'Or the European unladen swallow?'
+      ]
+    })
   ])
 
-  console.log(`seeded ${questions.length} questions`)
-  // console.log(`seeded ${forms.length} forms`)
+  const forms = await Promise.all([
+    Form.create(
+      {
+        stateCode: 'NY',
+        questions: [
+          {
+            questionType: 'input',
+            questionPrompt: 'What is your first name?',
+          },
+          {
+            questionType: 'paragraph',
+            questionPrompt: 'What is your address?',
+          }
+        ]
+      },
+      { 
+        include: [Question] 
+      }
+    )
+    .then(
+      (form) => form.addQuestions([
+        '73c7ed89-8c6f-4e6e-8d19-ca0250b37cb0',
+        '1ac4b921-0568-4fcc-af89-bf46b64f4068'
+      ])
+    ),
+    Form.create(
+      {
+        stateCode: 'CA',
+        questions: [
+          {
+            questionType: 'multiplechoice',
+            questionPrompt: 'what is your gender?',
+            possibleAnswers: [
+              'male-ish',
+              'female-ish',
+              'NB',
+              'HAHAHAHA!'
+            ]
+          }
+        ]
+      }, 
+      {
+        include: [Question]
+      }
+    )
+    .then(
+      (form) => form.addQuestions([
+        '73c7ed89-8c6f-4e6e-8d19-ca0250b37cb0',
+        '1ac4b921-0568-4fcc-af89-bf46b64f4068'
+      ])
+    ),
+  ])
+
+  // const answers = await 
+  
+  console.log(`seeded ${questions.length} basic questions`)
+  console.log(`seeded ${forms.length} form examples`)
+
   // console.log(`seeded ${users.length} users`)
   // console.log(`seeded ${users.length} users`)
   console.log(`seeded successfully`)
+
+  // const result = await Form.findOne({
+  //   where: { stateCode: 'CA' },
+  //   include: {
+  //     model: Question,
+  //     as: 'questions'
+  //   }
+  // })
+  // console.log(`attempting eager loading below`)
+  // // console.log(result)
+  // const q0 = result['dataValues']['questions'][0]['dataValues']['questionPrompt']
+  // const q1 = result['dataValues']['questions'][1]['dataValues']['questionPrompt']
+  // // const q2 = result['dataValues']['questions'][2]['dataValues']['questionPrompt']
+
+
+  // // console.log(q0,q1,q2)
+  // console.log(q0,q1)
 }
 
 // We've separated the `seed` function from the `runSeed` function.
@@ -65,5 +132,5 @@ if (module === require.main) {
   runSeed()
 }
 
-// we export the seed function for testing purposes (see `./seed.spec.js`)
+// we export the seed function for testing purposes
 module.exports = seed
