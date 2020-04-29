2cd35876afa8fc9bf509d6ac3a5fdd3993df49ab
cofuente <contact@cofuente.io>
2020-04-29 00:32:46 -0400

    added nifty methods testing for exploring

---
+++ server/utils/methods.js
@@ -0,0 +1,41 @@
+'use strict'
+
+const db = require('../db/_db')
+const {Form, Question, Answer, Submission} = require('../db/models')
+
+async function methodTest() {
+  await db.sync()
+  console.log('db synced!')
+
+  const result = await Form.findOne({
+    where: { stateCode: 'CA' },
+    include: {
+      model: Question,
+      as: 'questions'
+    }
+  })
+  console.log(`attempting eager loading below`)
+
+  console.log(result)
+
+}
+
+async function runMethodTest() {
+  console.log('seeding...')
+  try {
+    await methodTest()
+  } catch (err) {
+    console.error(err)
+    process.exitCode = 1
+  } finally {
+    console.log('closing db connection')
+    await db.close()
+    console.log('db connection closed')
+  }
+}
+
+if (module === require.main) {
+  runMethodTest()
+}
+
+module.exports = methodTest
