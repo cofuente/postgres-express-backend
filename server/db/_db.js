24185970e213bc2d920aae4cc46f8e964d448c29
cofuente <contact@cofuente.io>
2020-05-08 17:01:03 -0400

    added some signage

--- server/db/_db.js
+++ server/db/_db.js
@@ -3,6 +3,11 @@ const Sequelize = require('sequelize')
 
 const dbName = 'pebe'
 
+/* https://sequelize.org/v5/class/lib/sequelize.js~Sequelize.html#instance-constructor-constructor */
+
+
+console.log(process.env.DATABASE_URL, '<---------')
+// Passing a connection URI
 const db = new Sequelize( process.env.DATABASE_URL || `postgres://localhost:5432/${dbName}`, {
   define: {
     timestamps: true,
