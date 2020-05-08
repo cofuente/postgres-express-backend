e88d1b99d3257699a024ca2c0c7b3ac2e23f0012
cofuente <contact@cofuente.io>
2020-05-08 17:34:38 -0400

    trying something

--- server/db/_db.js
+++ server/db/_db.js
@@ -2,20 +2,20 @@
 const Sequelize = require('sequelize')
 const { Client } = require('pg')
 
-const client = new Client({
-  connectionString: process.env.DATABASE_URL,
-  ssl: true,
-})
+// const client = new Client({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: true,
+// })
 
-client.connect()
+// client.connect()
 
-client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
-  if (err) throw err
-  for (let row of res.rows) {
-    console.log(JSON.stringify(row))
-  }
-  client.end()
-})
+// client.query('SELECT table_schema,table_name FROM information_schema.tables;', (err, res) => {
+//   if (err) throw err
+//   for (let row of res.rows) {
+//     console.log(JSON.stringify(row))
+//   }
+//   client.end()
+// })
 
 const dbName = 'pebe'
 
