06d1eba1550ec5dac054a0ed17f3dd1817ad54cc
cofuente <contact@cofuente.io>
2020-05-14 23:28:26 -0400

    refactor and cleanup of server init

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const PORT = process.env.PORT || 1337
 const { db } = require('./server/db/models')
 
 
-db.sync()
-  .then(() => {
-    console.log('The postgres server is up and running - maybe you should go catch it!')
-		app.listen(PORT, (err) => {
-		  if (err) throw err
-		  console.log(`Your server kindly awaits your attention on port ${PORT}`)
-	  })
-	})
-	.catch(console.error)
+const init = async () => {
+  if (require.main === module){
+    //will only run when run with npm start and not with npm test to avoid db syncing in multiple threads when running tests
+    try {
+			await db.sync()
+			console.log('The postgres server is up and running - maybe you should go catch it!')
+      app.listen(PORT, () => {
+        console.log(`Your server kindly awaits your attention on port ${PORT}`)
+      })
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+init()
