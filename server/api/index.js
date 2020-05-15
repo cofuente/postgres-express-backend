01df19a741b6852a5e9558146b361d914674de69
cofuente <contact@cofuente.io>
2020-05-15 00:19:39 -0400

    added comments

--- server/api/index.js
+++ server/api/index.js
@@ -1,8 +1,12 @@
 
 const router = require('express').Router()
 
-// For now, we only need routes to GET forms or POST submissions
+// Ultimately, the client will only need routes to GET forms or POST submissions
 router.use('/forms', require('./form'))
 router.use('/submissions', require('./submission'))
+
+// additional routes for testing
+router.use('/questions', require('./question'))
+
 
 module.exports = router
