c4d5ef1d5e08b08cdb69e1f7ea9e0cfda3a0f9ca
cofuente <contact@cofuente.io>
2020-05-14 23:52:48 -0400

    rudimentary form route complete

--- server/api/form.js
+++ server/api/form.js
@@ -2,20 +2,63 @@ const router = require('express').Router()
 
 const { Form } = require('../db/models')
 
-router.param('id', (req, res, next, id) => {
-  Form.findById(id)
-    .then(form => {
-      if (!form) throw HttpError(404)
-      req.requestedForm = form
-      next()
-    })
-    .catch(next)
+
+// get all forms
+// Note: will eventually be deleted, only useful for development
+router.get('', async (req, res, next) => {
+  try {
+    const allForms = await Form.findAll()
+    if (allForms) res.status(200).json(allForms) 
+    else res.status(404).send('There are no forms in this database.')
+  } catch (error) {
+    next(error)
+  }
 })
 
-router.get('/', (req, res, next) => {
-  Form.findAll()
-    .then(forms => res.json(forms))
-    .catch(next)
+// make a new form, requires state code
+router.post('/', async (req, res, next) => {
+  try {
+    const newForm = await Form.create(req.body)
+    if (newForm) res.status(201).json(newForm)
+    else res.status(500).send('Something went wrong')
+  } catch (error) {
+    next(error)
+  } 
+})
+
+// get a specific form
+// TODO: alter this to include a form's questions
+router.get('/:formUUID', async (req, res, next) => {
+  try {
+    let requestedForm = await Form.findByPk(req.params.formUUID)
+    if (requestedForm) res.status(200).json(requestedForm) 
+    else res.status(404).send('form not found')
+  } catch (error) {
+    next(error)
+  }
+})
+
+// add a question to a form
+router.put('/:formUUID', async (req, res, next) => {
+  try {
+    const formToUpdate = await Form.findByPk(req.params.formUUID)
+    formToUpdate.addQuestions(req.body)
+    res.status(202).json(formToUpdate.formUUID)
+  } catch (error) {
+    next(error)
+  }
+  // req.requestedForm.addQuestions(req.body)
+  //   .then(form => res.status(202).json(form))
+  //   .catch(next)
+})
+
+router.delete('/:formUUID', async (req, res, next) => {
+  try {
+    await Form.destroy({ where: { formUUID: req.params.formUUID } })
+    res.status(204).send()
+  } catch (error) {
+    next(error)
+  }
 })
 
 module.exports = router
