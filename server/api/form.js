8f7fe81e25bb202df844a5b11210ee28f65e7f94
cofuente <contact@cofuente.io>
2020-06-17 13:51:45 -0400

    updated form route to allow removal of q's

--- server/api/form.js
+++ server/api/form.js
@@ -37,8 +37,8 @@ router.get('/:formUUID', async (req, res, next) => {
   }
 })
 
-// add a question to a form
-router.put('/:formUUID', async (req, res, next) => {
+// add a question or questions to a form
+router.put('/a/:formUUID', async (req, res, next) => {
   try {
     const formToUpdate = await Form.findByPk(req.params.formUUID)
     formToUpdate.addQuestions(req.body)
@@ -49,11 +49,22 @@ router.put('/:formUUID', async (req, res, next) => {
 })
 
 
+// remove a question or questions from a form
+router.put('/r/:formUUID', async (req, res, next) => {
+  try {
+    const formToUpdate = await Form.findByPk(req.params.formUUID)
+    formToUpdate.removeQuestions(req.body)
+    res.status(202).json(formToUpdate.formUUID)
+  } catch (error) {
+    next(error)
+  }
+})
+
 // delete a form
 router.delete('/:formUUID', async (req, res, next) => {
   try {
     await Form.destroy({ where: { formUUID: req.params.formUUID } })
-    res.status(204).send('deletion complete')
+    res.status(200).send('deletion complete')
   } catch (error) {
     next(error)
   }
