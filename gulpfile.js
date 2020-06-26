470a3c853609a44aaf2e72760f74d27ce6a91416
Mohawk Greene <kellyecodes@gmail.com>
2020-06-26 11:49:47 -0400

    Adds basic frontend

---
+++ gulpfile.js
@@ -0,0 +1,124 @@
+'use strict';
+
+/*
+ Including Gulp and plugins
+ */
+var gulp = require('gulp');
+var less = require('gulp-less');
+var sourcemaps = require('gulp-sourcemaps');
+var jade = require('gulp-jade');
+var beautify = require('gulp-jsbeautifier');
+var nano = require('gulp-cssnano');
+var rename = require('gulp-rename');
+var postcss = require('gulp-postcss');
+var mqpacker = require('css-mqpacker');
+var imagemin = require('gulp-imagemin');
+var pngquant = require('imagemin-pngquant');
+var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
+
+/*
+ Directory variables
+ */
+var lessSrcDir = './src/less/';
+var jadeSrcDir = './src/jade/';
+var assetsDir = './public/';
+var compiledStylesDir = assetsDir + 'stylesheets/';
+var libsDir = './libs/';
+
+/*
+ Compiling less, using sourcemaps
+ */
+gulp.task('less', function () {
+    gulp.src([
+        lessSrcDir + 'style.less'
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(less({
+          outputStyle: 'expanded'
+      }))
+      .pipe(sourcemaps.write())
+      .pipe(gulp.dest(compiledStylesDir));
+});
+
+/*
+ Creating a styles build file consisted of all files specified in gulp.src
+ */
+
+gulp.task('build:css', function () {
+    return gulp.src([
+        libsDir + '*.css'
+    ])
+        .pipe(concat('build.css'))
+        .pipe(postcss([mqpacker]))
+        .pipe(nano({
+            safe: true
+        }))
+        .pipe(rename('build.min.css'))
+        .pipe(gulp.dest(assetsDir + 'stylesheets'));
+});
+
+
+/*
+ Building compile jade, beautifying the output
+ */
+gulp.task('jade', function () {
+    gulp.src(
+        [
+            jadeSrcDir + 'next-distro-fe/index.jade'
+        ]
+    )
+        .pipe(jade())
+        .pipe(beautify({
+            html: {
+                indent_char: ' ',
+                indent_size: 4,
+                selector_separator_newline: true,
+                preserveNewlines: false,
+                indent_inner_html: true,
+                unformatted: ['span'],
+                extra_liners: ['section', 'footer'],
+                wrap_line_length: 0
+            }
+        }))
+        .pipe(gulp.dest('./public/'));
+});
+
+/*
+ Watching src files changes and running appropriate commands to compile them
+ */
+gulp.task('watch', function () {
+    gulp.watch([
+        lessSrcDir + '*.less'
+    ], ['less']);
+    gulp.watch([
+      jadeSrcDir + '*.jade',
+      jadeSrcDir + '**.jade',
+      jadeSrcDir + '**/**/*.jade'
+    ], ['jade']);
+});
+
+/*
+ Creating a JavaScript build consisted of all the files specified in gulp.src
+ */
+gulp.task('build:scripts', function () {
+    return gulp.src([
+        libsDir + 'jquery-2.1.1.min.js'
+    ])
+        .pipe(concat('build.js'))
+        .pipe(uglify())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest(assetsDir + 'build'));
+});
+
+/*
+ Optimising the images
+ */
+gulp.task('imagemin', function () {
+    return gulp.src('./public/images/**/*')
+        .pipe(imagemin({
+            progressive: true,
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('./public/images'));
+});
--- index.js
+++ index.js
@@ -2,7 +2,81 @@ const app = require('./server')
 const PORT = process.env.PORT || 1337
 const { db } = require('./server/db/models')
 const chalkAnimation = require('chalk-animation')
+const express = require('express')
+const path = require('path')
+const frontend = new express();
+const cors = require('cors')
+var axios = require('axios');
 
+const axios = require('axios');
+
+const data'
+
+axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY')
+  .then(response => {
+    data = response;
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+
+frontend.use(cors())
+frontend.use(express.json());
+frontend.use(express.urlencoded({ extended: true }))
+
+frontend.get('/', function(req,res){
+  res.status(200)
+    .json(data);
+});
+
+frontend.post('/', function(req,res){
+  var data = req.body;
+  console.log(data);
+});
+
+frontend.use('/home', express.static('views'));
+frontend.listen(9000);
+
+var users = [{
+  name: "Mohawk",
+  id: 0
+  },{
+    name: "Greene",
+    id: 1
+  }]
+
+frontend.route('/users')
+  .get(function(req,res){
+      res.status(200).json(users)
+  })
+  .post(function(req,res){
+      users.push(req.body)
+  })
+
+frontend.route('/users/:id')
+  .get(function(req,res){
+    console.log(req.params)
+    var user = users.filter(function(user){
+      return +user.id === +req.params.id
+    })[0];
+    if (user){
+      res.status(200).json(user);
+    }
+    if (!user) {
+      res.status(200).send();
+    }
+  })
+  .patch(function(req,res){
+    var user = users.filter(function(user){
+      return +user.id === +req.params.id
+    })[0];
+    var params = req.params
+    for (key in params) {
+      user[key] = params[key];
+    }
+  })
+  delete(function(){})
 
 const init = async () => {
   if (require.main === module){
