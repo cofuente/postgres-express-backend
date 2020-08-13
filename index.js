7591521a5f83b4568e298363e1be2ff3f82867b3
cofuente <contact@cofuente.io>
2020-08-13 19:00:10 -0400

    minor note on root index
    Signed-off-by: cofuente <contact@cofuente.io>

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const server = require('./server')
 const { db } = require('./server/db/models')
 const nextDistroEnrollmentForm = require('./client/utils/data.js')
   
+// TODO: possibly add require.main = module condition
 // Initialize server 
 const init = async () => {
   if (require.main === module){
-    //will only run when run with npm start and not with npm test to avoid db syncing in multiple threads when running tests
     try {
 			await db.sync()
       chalkAnimation.radar('The postgres server is up and running - maybe you should go catch it!', 2.58)
