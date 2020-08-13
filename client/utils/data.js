512c416fc6661936dd4c6141b94cc649b24e0705
Mohawk Greene <kellyecodes@gmail.com>
2020-08-13 11:18:48 -0400

    WIP

--- client/utils/data.js
+++ client/utils/data.js
@@ -1,6 +1,6 @@
 var axios = require('axios');
 
-module.exports.data = axios.get('https://postgress-express-backend.herokuapp.com/api/questions')
+module.exports.data = axios.get('https://postgress-express-backend.herokuapp.com/api/forms/eeae34c5-de07-4e1a-8402-602f158ca0a1')
   .then(response => {
     let form = response.data
     console.log("The API has loaded");
