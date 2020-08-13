86d897ce80e818f9ff139f702b0fb884e468291b
cofuente <contact@cofuente.io>
2020-08-13 19:01:44 -0400

    made data call dynamic
    Signed-off-by: cofuente <contact@cofuente.io>

--- client/utils/data.js
+++ client/utils/data.js
@@ -1,7 +1,10 @@
 var axios = require('axios');
 const chalkAnimation = require('chalk-animation')
 
-module.exports.data = axios.get('https://postgress-express-backend.herokuapp.com/api/questions')
+// TD: fix this environment identifier
+const current = process.env.NODE.slice(0,7) !== '/Users/' ? 'http://localhost:1337' : 'https://postgress-express-backend.herokuapp.com'
+
+module.exports.data = axios.get(`${current}/api/questions`)
   .then(response => {
     let form = response.data
     setTimeout(() => chalkAnimation.rainbow("The frontend has successfully loaded data from the API"), 4000)
