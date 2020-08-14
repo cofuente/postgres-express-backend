fee32b91fbe311658a5975b35d359f8683d77012
cofuente <contact@cofuente.io>
2020-08-14 14:25:16 -0400

    added TD
    Signed-off-by: cofuente <contact@cofuente.io>

--- server/utils/seed.js
+++ server/utils/seed.js
@@ -47,6 +47,7 @@ async function runSeed() {
   }
 }
 
+// TD: check if below is still true
 // Execute the `seed` function, IF we ran this module directly (`node seed`).
 // `Async` functions always return a promise, so we can use `catch` to handle
 // any errors that might occur inside of `seed`.
