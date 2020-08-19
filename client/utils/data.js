5c31b1ed9f10becec5ae95252097fc520019a994
cofuente <contact@cofuente.io>
2020-08-19 15:29:14 -0400

    cleaned up unused log highlighter
    Signed-off-by: cofuente <contact@cofuente.io>

--- client/utils/data.js
+++ client/utils/data.js
@@ -1,6 +1,4 @@
 var axios = require('axios');
-const chalkAnimation = require('chalk-animation')
-
 const current = process.env.NODE !== '/app/.heroku/node/bin/node' ? 'http://localhost:1337' : 'https://postgress-express-backend.herokuapp.com'
 
 module.exports = axios.get(`${current}/api/questions`)
